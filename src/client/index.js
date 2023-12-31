e488e0300906d71a71c7c66140b9d9333d422fc1
William Castro <william.castro2000@outlook.com>
2023-12-30 19:57:06 -0500

    MAJOR UPDATE: Starting the implmenentation of my project to firebase

---
+++ src/client/index.js
@@ -0,0 +1,36 @@
+//Firebase.Google: Getting started with Firebase for the web Video on YouTube & on the Google Firebase site.
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js'; //'firebase/app';
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';   //'firebase/auth';
+import { getFirestore, collection, getDocs, getDocs, getDoc } from 'firebase/firestore';
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCk1c7S1-SRayX5x402qRt6Kmv1eXTkhJI",
+    authDomain: "alpha-note-274ac.firebaseapp.com",
+    projectId: "alpha-note-274ac",
+    storageBucket: "alpha-note274ac.appspot.com",
+    messagingSenderId: "683733177535",
+    appId: "1:683733177535:web:3a3312c51ab980863ce20a",
+    measurementId: "G-TV6C3Q16HT"
+});
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+const todosCol = collection(db, 'todos');
+const snapshot = await getDocs(todosCol);
+
+// Detect auth state
+
+auth.onAuthStateChanged(user => {
+
+});
+onAuthStateChanged(auth, user => {
+    if (user != null) {
+        console.log('logged in!');
+    } else {
+        console.log('NO --- USER!');
+    }
+});
+
+
+
